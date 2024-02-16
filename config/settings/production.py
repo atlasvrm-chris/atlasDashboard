06abd2b8195bffe02e50ba2cba984ad768cc7f1f
Chris Shrimpton <146737288+atlasvrm-chris@users.noreply.github.com>
2024-02-15 20:10:41 -0700

    Add Heroku domain to ALLOWED_HOSTS

--- config/settings/production.py
+++ config/settings/production.py
@@ -4,7 +4,7 @@ from .base import env
 # GENERAL
 # ------------------------------------------------------------------------------
 SECRET_KEY = env("DJANGO_SECRET_KEY")
-ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["yourdomain.com"])
+ALLOWED_HOSTS = env.list("DJANGO_ALLOWED_HOSTS", default=["atlas-dashboard-05035db01019.herokuapp.com"])
 
 # DATABASES
 # ------------------------------------------------------------------------------
